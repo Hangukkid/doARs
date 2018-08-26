d5e4f3218cfd27c935054894f9faee357be7d20c
Soo Yeon Cho <sooyeon1998@gmail.com>
2018-08-26 02:32:21 -0400

    Completed search
    - appearing and disappearing search
    - global variables that store search terms for searching doars

---
+++ Unity/Assets/Scripts/SearchBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchBar : MonoBehaviour
+{
+    public GameObject bar;
+    public bool buttonSearch = true;
+
+    private void Start()
+    {
+        //buttonSearch = true;
+        bar = GameObject.Find("InputField");
+    }
+
+    public void Button_Click()
+    {
+        //if (bar == null) Handheld.Vibrate();
+        if (!buttonSearch) { // if user decides to search
+            //Handheld.Vibrate();
+            buttonSearch = true;
+            bar.SetActive(true);
+            
+            //search for stuff
+        }
+        else if (buttonSearch) { //if the user searched
+            buttonSearch = false;
+            bar = GameObject.Find("InputField");
+            bar.SetActive(false);
+
+            //Handheld.Vibrate();
+        }
+    }
+
+    public void Text_Changed(string newText)
+    {
+        doARsState.search = newText;
+        doARsState.newSubmission = true;
+    }
+}
--- Unity/Assets/Scripts/doARsState.cs
+++ Unity/Assets/Scripts/doARsState.cs
@@ -21,6 +21,7 @@ public static class doARsState
     public static List<string> found_worlds;
 
     public static string search = null;
+    public static bool newSubmission = false;
 
     public static doARs_state getNextState()
     {
