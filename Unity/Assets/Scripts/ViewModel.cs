bcd0f790d2fb009723bf049d5bd35b4c6848d61c
Soo Yeon Cho <sooyeon1998@gmail.com>
2018-08-25 23:40:20 -0400

    Search bar (not tested)
    Global variable for search result added

---
+++ Unity/Assets/Scripts/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ViewModel : MonoBehaviour 
+{
+    public Text buttonText;
+    public GameObject cube;
+    public Slider slider;
+
+    public void Button_Click()
+    {
+        Debug.Log("Hello, World!!!!!!!");
+    }
+
+    public void Text_Changed(string newText)
+    {
+        Debug.Log(newText);
+    }
+
+}
